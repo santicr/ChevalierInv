7ac5dcbf29b4fca3a98444e22571067cbf9c9311
santicr <santicr@javerianacali.edu.co>
2022-12-30 13:52:18 -0500

    Agregado: Migraciones de cada app, barber y appointment, se agregaron la opciones de barbero

--- mysite/appointment/forms.py
+++ mysite/appointment/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from barber.models import Barber
 
 class ReserveForm(forms.Form):
+    choices = Barber.objects.all()
+    choices = [(b, b) for b in choices]
     name = forms.CharField(label = 'Nombre completo', max_length = 100, required = True, widget = forms.TextInput(
         attrs = {'type': 'text', 'class': 'form-control'}
     ))
@@ -19,3 +22,4 @@ class ReserveForm(forms.Form):
     hour = forms.TimeField(label = 'Selecciona la hora disponible', required = True, widget = forms.TimeInput(
         attrs = {'type': 'time', 'min': '8:00', 'max': '18:00'}
     ))
+    barber = forms.ChoiceField(choices = choices, required = True, label = 'Barbero')
