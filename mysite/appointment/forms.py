2a615911f660f0d3f0c80597e0997c6855eba15d
santicr <santicr@javerianacali.edu.co>
2022-12-30 12:05:22 -0500

    Añadido: formulario para poder reservar, sin embargo, es necesario AJUSTAR

---
+++ mysite/appointment/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+class ReserveForm(forms.Form):
+    name = forms.CharField(label = 'Nombre completo', max_length = 100, required = True, widget = forms.TextInput(
+        attrs = {'type': 'text', 'class': 'form-control'}
+    ))
+    lastname1 = forms.CharField(label = 'Apellido paterno', max_length = 100, required = True, widget = forms.TextInput(
+        attrs = {'type': 'text', 'class': 'form-control'}
+    ))
+    lastname2 = forms.CharField(label = 'Apellido materno', max_length = 100, required = True, widget = forms.TextInput(
+        attrs = {'type': 'text', 'class': 'form-control'}
+    ))
+    email = forms.EmailField(label = 'Correo electronico', required = True, widget = forms.EmailInput(
+        attrs = {'type': 'email', 'class': 'form-control'}
+    ))
+    date = forms.DateTimeField(label = 'Fecha para peluquearse', required = True, widget = forms.DateTimeInput(
+        attrs = {'type': 'date', 'min': "2023-01-01", 'max': "2023-12-31", 'lang': 'fr-CA'}
+    ))
+    hour = forms.TimeField(label = 'Selecciona la hora disponible', required = True, widget = forms.TimeInput(
+        attrs = {'type': 'time', 'min': '8:00', 'max': '18:00'}
+    ))
--- mysite/appointment/views.py
+++ mysite/appointment/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .forms import ReserveForm
 
 # Create your views here.
+def reserve(req):
+    form = ReserveForm()
+    return render(req, 'appointment/reserve.html', {'form': form})
