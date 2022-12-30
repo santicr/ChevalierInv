21cc0f2e4043756faedfb31cde75fd2128b0e6f2
santicr <santicr@javerianacali.edu.co>
2022-12-30 15:36:32 -0500

    Cambio: Formulario de reserva de citas, se muestran las horas desde las 8:30am hasta las 8:00pm cada 30 minutos

--- mysite/appointment/forms.py
+++ mysite/appointment/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from barber.models import Barber
+from .models import Appointment
+from datetime import datetime, timedelta, date
 
 class ReserveForm(forms.Form):
-    choices = Barber.objects.all()
-    choices = [(b, b) for b in choices]
+    barber_choices = Barber.objects.all()
+    barber_choices = [(b, b) for b in barber_choices]
+    first_hour = timedelta(hours = 8, minutes = 30)
+    time_choices = [(i, timedelta(hours = 8, minutes = 30) + timedelta(minutes = 30 * i)) for i in range(23)]
+
     name = forms.CharField(label = 'Nombre completo', max_length = 100, required = True, widget = forms.TextInput(
         attrs = {'type': 'text', 'class': 'form-control'}
     ))
@@ -16,10 +21,8 @@ class ReserveForm(forms.Form):
     email = forms.EmailField(label = 'Correo electronico', required = True, widget = forms.EmailInput(
         attrs = {'type': 'email', 'class': 'form-control'}
     ))
-    date = forms.DateTimeField(label = 'Fecha para peluquearse', required = True, widget = forms.DateTimeInput(
+    date = forms.DateTimeField(label = 'Fecha de reserva', required = True, widget = forms.DateTimeInput(
         attrs = {'type': 'date', 'min': "2023-01-01", 'max': "2023-12-31", 'lang': 'fr-CA'}
     ))
-    hour = forms.TimeField(label = 'Selecciona la hora disponible', required = True, widget = forms.TimeInput(
-        attrs = {'type': 'time', 'min': '8:00', 'max': '18:00'}
-    ))
-    barber = forms.ChoiceField(choices = choices, required = True, label = 'Barbero')
+    hour = forms.ChoiceField(choices = time_choices, required = True, label = 'Hora')
+    barber = forms.ChoiceField(choices = barber_choices, required = True, label = 'Barbero')
--- mysite/appointment/views.py
+++ mysite/appointment/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ReserveForm
+from datetime import datetime, timedelta, date
 
 # Create your views here.
 def reserve(req):
     form = ReserveForm()
+    if req.method == 'POST':
+        data = req.POST
+        form = ReserveForm(data)
+
+        if form.is_valid():
+            name = data['name']
+            lname1 = data['lastname1']
+            lname2 = data['lastname2']
+            email = data['email']
+            date = data['date']
+            hour = data['hour']
+            barber = data['barber']
+
+        return redirect('index')
+    
     return render(req, 'appointment/reserve.html', {'form': form})
