a6713fd9593eff687acea46ec114497c155655e6
santicr <santicr@javerianacali.edu.co>
2023-01-06 07:41:35 -0500

    Añadido: Hora y llave foranea

--- mysite/barber/models.py
+++ mysite/barber/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Hour(models.Model):
+    hour = models.TimeField()
+
 class Barber(models.Model):
     name = models.CharField(max_length = 100)
     lastname1 = models.CharField(max_length = 100)
     lastname2 = models.CharField(max_length = 100)
+    hour = models.ForeignKey(Hour, on_delete = models.PROTECT)
 
     def __str__(self) -> str:
         return f'{self.name} {self.lastname1} {self.lastname2} {self.id}'
